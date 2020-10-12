0b3a760ad0a8ac890790a20e4ce462a91c33bbfc
Dhairya_prj <dhairyahp15@gmail.com>
2020-10-12 05:47:15 -0500

    Actually, the test file was totally out of my mind so I updated it now and everyhting works properly to this moment except the moving tile logic.

--- TestClassA2.java
+++ TestClassA2.java
@@ -6,9 +6,9 @@ public class TestClassA2{
   public static void main(String[] args){
   	phase1();
   	StdDraw.show(1000);  // Draw all art and pause for 1 second
-  //	phase2();
+  	phase2();
   	StdDraw.show(1000); // Draw all art and pause for 1 second
-  //	phase3();
+  	phase3();
   }
 
   // Should be a board with one red and one black tile in the corners (see images. )
@@ -16,15 +16,15 @@ public class TestClassA2{
     Board b = new Board();
     // testing out all the instance methods created.
     b.drawBoard();
-    //b.drawTile(0, 0, Board.red);
-    //b.drawTile(7, 7, Board.black);
-    b.resetBoard();
-    b.drawTiles();
+    b.drawTile(0, 0, Board.red);
+    b.drawTile(7, 7, Board.black);
+    //b.resetBoard();
+    //b.drawTiles();
     //b.clearBoard();
-    b.setTile(4, 6, Board.black);
-    b.drawTiles();
+    //b.setTile(4, 6, Board.black);
+    //b.drawTiles();
   }
-/*
+
   // Should be the board set up with tiles 
   // Note this is just drawing them, it is not setup. 
   public static void phase2(){
@@ -51,5 +51,5 @@ public class TestClassA2{
     StdDraw.show(1000);
     // Make sure it is being done correctly. 
   }
-  */
+  
 }
