97c6321d3190f47086735d53290695be84e3e77d
Dhairya_prj <dhairyahp15@gmail.com>
2020-10-16 18:27:28 -0500

    Refactorized and commented the code in the test class and Board class. And removed some redundant code. No more changes to be done, I guess. Ready to submit.

--- Board.java
+++ Board.java
@@ -1,8 +1,7 @@
-    /**
- * Checkers game base framework. 
- */
+/*
+* Checkers game base framework. 
+*/
 public class Board {
-  
   // board size - square tiles
   // Size of the board (per size) in units
   public static final int BOARD_SIZE = 8;
@@ -19,8 +18,6 @@ public class Board {
   // The state of the board
   public String[][] boardState;
   
-  // Initialize the board state 
-  // Set all values of array to Empty
   // Phase 1
   public Board(){
     // Add your code here .... 
@@ -43,8 +40,8 @@ public class Board {
         else  
           StdDraw.setPenColor(StdDraw.WHITE);
         // updating center for every checks so that it draws a column of checks for every row.
-        double xC = (2 * j + 1)/(2.0 * BOARD_SIZE);
-        double yC = (2 * i + 1)/(2.0 * BOARD_SIZE);   
+        double xC = (2 * j + 1)/(2.0 * BOARD_SIZE); // center x coordinate of corresponding tile.
+        double yC = (2 * i + 1)/(2.0 * BOARD_SIZE); // center x coordinate of corresponding tile.
         //drawing filled rectangle with the corresponding color of the pen.
         StdDraw.filledRectangle(xC, yC, TILE_RADIUS, TILE_RADIUS);
       }    
@@ -60,8 +57,8 @@ public class Board {
   public void drawTile(int row, int col, String color)
   {
       // Scaling center of the tile using the given row and column.
-    double xC = (2 * col + 1)/(2.0 * BOARD_SIZE);
-    double yC = (2 * row + 1)/(2.0 * BOARD_SIZE);
+    double xC = (2 * col + 1)/(2.0 * BOARD_SIZE); // center x coordinate of corresponding tile.
+    double yC = (2 * row + 1)/(2.0 * BOARD_SIZE); // center x coordinate of corresponding tile.
     // This should work only if the given String color is red or black.
     if(color.equalsIgnoreCase(red) || color.equalsIgnoreCase(black))
     {
@@ -78,7 +75,7 @@ public class Board {
   // This method verifies if the tile is valid or not.
   public boolean isValidTile(int row, int col)
   {
-    boolean isValid  = false;
+    boolean isValid  = false; // boolean variable that is to be returned for valid tile as true or false.
     if((row + col) % 2 == 0)
       isValid = true;
     return isValid;
@@ -88,7 +85,7 @@ public class Board {
   public void resetBoard()
   {
     clearBoard(); // Clears the board.
-    for(int i = 0; i < 3; i++)
+    for(int i = 0; i < 3; i++) // for loop for first 3 rows, to be filled with red tiles.
     {
       for(int j = 0; j < BOARD_SIZE; j++)
       {
@@ -101,7 +98,7 @@ public class Board {
 
     for(int i = BOARD_SIZE - 1; i >= BOARD_SIZE - 3; i--)
     {
-      for(int j = 0; j < BOARD_SIZE; j++)
+      for(int j = 0; j < BOARD_SIZE; j++) // for loop for last 3 rows, to be filled with black tiles.
       {
         if(isValidTile(i, j))
         {
@@ -132,6 +129,7 @@ public class Board {
   // ClearBoard() method clears the state of board, i.e. set all the values of boardState to empty.
   public void clearBoard()
   {
+    // creating new instance for boardState and setting all values to empty.
     boardState = new String[BOARD_SIZE][BOARD_SIZE];
     for(int i = 0; i < BOARD_SIZE; i++)
     {
@@ -142,23 +140,26 @@ public class Board {
     }
   } 
 
+  // This method returns the color of the corresponding row and column. 
   public String getTileColor(int row, int col)
   {
     return boardState[row][col];
-  }
+  } 
 
   public void drawSelectedTile(int row, int col)
   {
-    double xC = (2 * col + 1)/(2.0 * BOARD_SIZE);
-    double yC = (2 * row + 1)/(2.0 * BOARD_SIZE);
+    double xC = (2 * col + 1)/(2.0 * BOARD_SIZE); // center x coordinate of corresponding tile.
+    double yC = (2 * row + 1)/(2.0 * BOARD_SIZE); // center y coordinate of corresponding tile.
     StdDraw.setPenColor(StdDraw.GREEN);
     StdDraw.ellipse(xC, yC, TILE_RADIUS, TILE_RADIUS);
-  }
+  } // This method draws green circle around the selected tile, to let the user know which tile is selected
 
+  /* This method is used to count the number of tiles of the given color left
+  to decide the winner of the game. */
   public int countTiles(String color)
   {
-    int count = 0;
-    for(int i = 0; i < BOARD_SIZE; i++)
+    int count = 0; // count variable to be increased when corresponding color found.
+    for(int i = 0; i < BOARD_SIZE; i++) // looping through whole boardState.
     {
       for(int j = 0; j < BOARD_SIZE; j++)
       {
@@ -169,14 +170,18 @@ public class Board {
     return count;
   }
 
+  /* This method checks wheter the move is valid or not, i.e. it is 
+  moving diagonally and only in the gray tiles as well as if there is 
+  enemy in the diagonal, it should move 2 steps diagonally. */
   public boolean isValidMove(int startRow, int startCol, int endRow, int endCol, String color)
   {
-    boolean isValidMove = false;
-    if(isValidTile(endRow, endCol))
+    boolean isValidMove = false; // boolean variable that is supposed to be returned as valid move or not
+    // if the ending tile is valid and empty.
+    if(isValidTile(endRow, endCol)) 
     {
       if(boardState[endRow][endCol].equals(empty))
       {
-        if(color.equals(red))
+        if(color.equals(red)) // logic for red color tiles
         {
           if(endRow - startRow == 1 && (endCol - startCol == 1 || startCol - endCol == 1))
             isValidMove = true;
@@ -191,7 +196,7 @@ public class Board {
             isValidMove = false;
       }
 
-      if(color.equals(black))
+      if(color.equals(black)) // logic for black color tiles
       {
           if(startRow - endRow == 1 && (endCol - startCol == 1 || startCol - endCol == 1))
              isValidMove = true;
@@ -207,18 +212,23 @@ public class Board {
             isValidMove = false;
         }
       }
-      else
+      else // if the end tile is not empty then it is not a valid move.
         isValidMove = false;
     }
-    return isValidMove;
+    return isValidMove; // returns the boolean for valid move or not
   }
 
+  /* This method is used to apply the move, i.e. move the tile to the given
+  end row and end col which is to be used in GameController, it will also
+  check for isValidMove, so we do not have to check for isValidMove here.*/ 
   public void applyMove(int startRow, int startCol, int endRow, int endCol)
   {
-    if(isValidTile(endRow, endCol))
+    if(isValidTile(endRow, endCol)) // if endRow and endCol is gray tile.
     {
+      // condition for red color tile.
       if(boardState[startRow][startCol].equals(red))
       {
+        // swapping and capturing logic of tile from startRow startCol to endRow endCol.
         if(endRow - startRow == 1 && (endCol - startCol == 1 || startCol - endCol == 1))
         {
           boardState[endRow][endCol] = boardState[startRow][startCol];
@@ -234,8 +244,9 @@ public class Board {
               boardState[endRow - 1][endCol - 1] = empty;
         }
       }
-      else
+      else // else part for black color tile.
       {
+        // swapping and capturing logic of tile from startRow startCol to endRow endCol.
         if(startRow - endRow == 1 && (endCol - startCol == 1 || startCol - endCol == 1))
         {
           boardState[endRow][endCol] = boardState[startRow][startCol];
@@ -253,4 +264,4 @@ public class Board {
       }
     }
   }
-}//Chessboard
+} //Chessboard
--- TestClassA2.java
+++ TestClassA2.java
@@ -23,43 +23,48 @@ public class TestClassA2{
   }
 
   // Should be a board with one red and one black tile in the corners (see images. )
+  // Phase 1 tests.
   public static void phase1(){
     Board b = new Board();
     // testing out all the instance methods created.
     System.out.println("Drawing Board, and tile at [0][0] & [7][7].");
-    b.drawBoard();
-    b.drawTile(0, 0, Board.red);
-    b.drawTile(7, 7, Board.black);
+    b.drawBoard(); // draws empty board.
+    b.drawTile(0, 0, Board.red); // drwaing tile at 0,0.
+    b.drawTile(7, 7, Board.black);// drawing tile at 7,7.
   }
-
-  // Should be the board set up with tiles 
-  // Note this is just drawing them, it is not setup. 
+ 
   public static void phase2(){
-    Board b = new Board();
+    Board b = new Board(); // new Board instance.
 
-    b.resetBoard();
-    System.out.println("Tile at row -> 1, and column -> 3 is Valid: " + b.isValidTile(1, 3));
-
-  }
-
-  // This includes using a Board state that can be stored and updated. 
-  public static void phase3(){
-  	// default board
-    Board b = new Board();
-    System.out.println("Resetting board and drawing Tiles to the default position.");
-    b.resetBoard();
+    b.resetBoard(); // resetBoard() method sets every tile to its initial position.
+    // Drawing board and tiles to see the current state of the board.
     b.drawBoard();
     b.drawTiles();
+    // Testing isValidTile().
+    System.out.println("Tile at row -> 1, and column -> 3 is Valid: " + b.isValidTile(1, 3));
+  }
+
+  // Phase 3 tests.
+  public static void phase3(){
+  	// default board
+    Board b = new Board(); // new Board instance
+    System.out.println("Resetting board and drawing Tiles to the default position.");
+    b.resetBoard(); // resets every tiles to default position.
+    b.drawBoard();
+    b.drawTiles(); // drawing current position of tiles on the board.
     StdDraw.show(1000); // Draw all art and pause for 1 second
 
-    b = new Board();
+    b = new Board(); // again initializing Board instance.
+    // Testing all other mthods fir phase 3.
     System.out.println("Initializing board again and using setTile to set tiles and drawTiles to draw on the board.");
-    b.setTile(0,2,"RED");
-    b.setTile(7,5,"BLACK");
-    b.drawBoard();
+    b.setTile(0,2,"RED"); // setting red tile to 0,2. If valid.
+    b.setTile(7,5,"BLACK"); // setting black tile to 7,5. If valid.
+    // drawing blank board and then drawing current position of tiles to see the changes.
+    b.drawBoard(); 
     b.drawTiles();
     StdDraw.show(1000); // Draw all art and pause for 1 second
     System.out.println("Cleared Board using clearBoard():");
+    // Testing clearBoard() method.
     b.clearBoard();
     b.drawBoard();
     b.drawTiles();
@@ -67,35 +72,41 @@ public class TestClassA2{
     // Make sure it is being done correctly. 
   }
 
+  // Phase 4 tests.
   public static void phase4()
   {
-    Board b = new Board();
+    Board b = new Board(); // new Board instance.
     //b.clearBoard();
     b.drawBoard();
     b.resetBoard();
     b.drawTiles();
+    // testing out all phase 4 methods using print statements. 
     System.out.println("Tile at row -> 0, and column -> 0 is: "+b.getTileColor(0, 0));
     System.out.println("Tile at row -> 4, and column -> 4 is: "+b.getTileColor(4, 4));
     System.out.println("Drawing selected tile [7][7] using draw selected method:" );
-    b.drawSelectedTile(7, 7);
+    b.drawSelectedTile(7, 7); // this will draw green circle around the selected tile.
     System.out.println("Total number of Black tiles: "+b.countTiles("BLACK"));
   }
   
+  // phase 5 tests.
   public static void phase5()
   {
-    Board b = new Board();
+    Board b = new Board(); // new Board instance
     b.drawBoard();
     b.resetBoard();
     b.drawTiles();
+    // Testing out all the methods for phase 5. 
     System.out.print("Moving red Tile to one of the White check on the board, ");
     System.out.println("isValidMove - " + b.isValidMove(2, 0, 3, 0, "RED"));
-    b.applyMove(2, 0, 3, 0);
+    b.applyMove(2, 0, 3, 0); // This will apply move if it is valid.
+    // Drawing board and tiles again to see the changes.
     b.drawBoard();
     b.drawTiles();
     StdDraw.show(1000); // Draw all art and pause for 1 second
     System.out.print("Moving black Tile to one of the White check on the board, ");
     System.out.println("isValidMove - " + b.isValidMove(5, 1, 4, 0, "BLACK"));
-    b.applyMove(5, 1, 4, 0);
+    b.applyMove(5, 1, 4, 0); // This will apply move if it is valid.
+    // Drawing board and tiles again to see the changes.
     b.drawBoard();
     b.drawTiles();
     StdDraw.show(1000); // Draw all art and pause for 1 second
